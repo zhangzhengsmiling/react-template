eb58a9a84bce891b2e4172b8be79f1fe580c818f
zhangzhengsmiling <zhangzhengsmiling@163.com>
2022-04-02 11:14:51 +0800

    feat: babel pipeline operator

--- ice.config.js
+++ ice.config.js
@@ -9,13 +9,35 @@ module.exports = (config) => {
   const loaderCssModule = config.module.rules[9];
   loaderCssModule.include = [loaderCssModule.include, path.resolve(__dirname, 'node_modules')];
   loaderCss.include = [loaderCss.include, path.resolve(__dirname, 'node_modules')];
+  console.log(config.module.rules[1].use[1]);
   const _config = {
     entry: path.resolve(__dirname, './src/index.tsx'),
     output: {
       path: path.resolve(__dirname, 'build'),
       filename: 'bundle.[chunkhash:8].js',
     },
+    module: {
+      rules: [
+        {
+          test: /.tsx$|.ts$/,
+          include: /src/,
+          use: [
+            {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
+                plugins: [
+                  [
+                    '@babel/plugin-proposal-pipeline-operator',
+                    { topicToken: '^^', proposal: 'fsharp' },
+                  ],
+                ],
+              },
+            },
+          ],
+        },
+      ],
+    },
   };
-
   return merge(config, _config);
 };
--- src/pages/TestPage.tsx
+++ src/pages/TestPage.tsx
@@ -3,6 +3,8 @@ import './style';
 import '../a.css';
 
 const TestPage = () => {
+  // 10 |>
+  //   console.log
   return (
     <div className="test-page">
       this is test page...
